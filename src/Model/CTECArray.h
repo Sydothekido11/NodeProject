a806d210c32d62a591e68910c626797f406a9cc6
Sydothekido11 <snem8901@740s-pl.local>
2016-02-02 12:37:57 -0700

    Added a CTECArray.

---
+++ src/Model/CTECArray.h
@@ -0,0 +1,17 @@
+/*
+ * CTECArray.h
+ *
+ *  Created on: Feb 2, 2016
+ *      Author: snem8901
+ */
+
+#ifndef MODEL_CTECARRAY_H_
+#define MODEL_CTECARRAY_H_
+
+class CTECArray {
+public:
+	CTECArray();
+	virtual ~CTECArray();
+};
+
+#endif /* MODEL_CTECARRAY_H_ */
