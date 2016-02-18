6b22514c39c405054841b7a535b1356298055b54
Sydothekido11 <snem8901@740s-pl.local>
2016-02-18 12:35:23 -0700

    Fixed the assert error.

--- src/Model/CTECArray.cpp
+++ src/Model/CTECArray.cpp
@@ -5,96 +5,40 @@
  *      Author: snem8901
  */
 
-#include "CTECArray.h"
-using namespace std;
 
-/**
- * Creates an array that has to do with size. Also creates a defense so the area is not below a size 0. Made a first ArrayNode, the head.
- */
-template <class Type>
+#include "CTECArray.h"
+#include<iostream>
+#include<assert.h>
+using namespace std;
+template<class Type>
 CTECArray<Type>::CTECArray(int size)
 {
 	this->size = size;
-	this->head = nullptr;
-
-	//Defensive
-	assert(size > 0);
-
-
+	this->size = size;
+		this->head = nullptr;
+		//defensive code
+		if(size <= 0 )
+		{
+			cerr << "WHAAAAAAAAAAAAAT?!" << endl;
+			return;
+		}
 	for(int index = 0; index < size; index++)
 	{
-
 		if(head != nullptr)
-		{
-			//Regular ArrayNodes are being made.
-			ArrayNode<Type> * nextNode = new ArrayNode<Type>();
-			nextNode->setNext(head);
-			this->head = &nextNode;
-		}
-		else
-		{
-			//The first ArrayNode needs to be made.
-			ArrayNode<Type> * firstNode = new ArrayNode<Type>();
-			this->head = firstNode;
-
-		}
+				{
+					ArrayNode<Type> * nextNode = new ArrayNode<Type>();
+					nextNode->setNext(head);
+					this->head = nextNode;
+				}
+				else
+				{
+					ArrayNode<Type>* firstNode = new ArrayNode<Type>();
+					this->head = firstNode;
+				}
 	}
-}
-/**
- * Creates a reminder when there is an out of bounds error.
- */
-template <class Type>
-Type CTECArray<Type> :: get(int position)
-{
-	//We need to do bounds checking so we do not crash the program.
-	assert(position < size && position >= 0);
-	ArrayNode<Type> * current = head;
-	for(int spot = 0; spot <= position; spot++)
-	{
-		if(spot != position)
-		{
-			current = current->getNext();
-		}
-		else
-		{
-			return current->getValue();
-		}
-	}
-}
- /**
-  * Returns the size of the array.
-  */
-template <class Type>
-int CTECArray<Type> :: getSize()
-{
-	return this->size;
-}
 
-/**
- * Finds the out of bounds error.
- */
-template <class Type>
-void CTECArray<Type> :: set(int position, const Type& value)
-{
-	assert(position < size && position >=0);
-	ArrayNode<Type> * current = head;
-	for(int spot = 0; spot <= position; spot++)
-	{
-		if(spot != position)
-		{
-			current = current->getNext();
-		}
-		else
-		{
-			current->setValue(value);
-		}
-	}
 }
-
-/**
- * This is a de-constructor. Removes the head and the rest of the array when it is no longer needed.
- */
-template <class Type>
+template<class Type>
 CTECArray<Type>::~CTECArray()
 {
 	ArrayNode<Type> * deleteMe = head;
@@ -116,5 +60,54 @@ CTECArray<Type>::~CTECArray()
 
 	delete head;
 }
+
+
+
+template<class Type>
+Type CTECArray<Type>::get(int pos)
+{
+	assert(pos < size && pos >= 0);
+		ArrayNode<Type> * current = head;
+		for (int spot = 0; spot <= pos; spot++)
+		{
+			if (spot != pos)
+			{
+				current = current->getNext();
+			}
+			else
+			{
+				return current->getValue();
+			}
+		}
+
+}
+
+template<class Type>
+int CTECArray<Type>::getSize()
+{
+	return this->size;
+}
+
+template<class Type>
+void CTECArray<Type>::set(int pos, const Type& value)
+{
+	assert(pos < size && pos >= 0);
+		ArrayNode<Type> * current = head;
+		for (int spot = 0; spot <= pos; spot++)
+		{
+			if (spot != pos)
+			{
+				current = current->getNext();
+			}
+			else
+			{
+				current->setValue(value);
+			}
+
+		}
+
+}
+
+
 
 
